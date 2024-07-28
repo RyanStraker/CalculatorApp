b86b27035bb65ec31e0c0b17fece29946f709e0d
Ryan <ryanstraker@proton.me>
2024-07-28 17:25:35 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function() {
+    console.log("hiya pet");
+    
+    var containerBody = document.getElementById('container-body');
+
+    var width = screen.availWidth;
+
+    var setMarginTop = function () {
+        var width = screen.availWidth;
+    }
+})
