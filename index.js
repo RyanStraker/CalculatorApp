12a9bf6b52d5c71836d0db41d7910d7b4729124a
Ryan <ryanstraker@proton.me>
2024-08-01 10:26:47 +0100

    CSS 90% complete. Basic JS down

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 window.addEventListener('DOMContentLoaded', function() {
     console.log("hiya pet");
-    
-    var containerBody = document.getElementById('container-body');
 
-    var width = screen.availWidth;
+    //preliminary variable definitions//
 
-    var setMarginTop = function () {
-        var width = screen.availWidth;
+    var numbersDisplay = document.getElementById('numbers-display');
+    var calculatorDisplayFigure = "";
+
+    //code that powers the calculator function: 1. creating an array from the HTMLCollection containing all number buttons on the calculator and updating the calculator interface whenever they are pressed//
+
+    var numberButtons = document.getElementsByClassName('number');
+
+    var numberKeysArrayOriginal = [];
+
+    for (var i = 0; i < numberButtons.length; i++) {
+        numberKeysArrayOriginal.push(numberButtons[i]);
     }
+
+    var numberButtonsListening = numberKeysArrayOriginal.map(function(value) {
+        value.addEventListener('click', function(event) {
+            if (event.target.innerHTML == "RESET") {
+                calculatorDisplayFigure = "";
+                numbersDisplay.innerHTML = calculatorDisplayFigure;
+            } else if (event.target.innerHTML == "DEL" && calculatorDisplayFigure.length >= 1) {
+                var replacementDisplay = "";
+                for (var i = 0; i != calculatorDisplayFigure.length - 1; i++) {
+                    replacementDisplay += calculatorDisplayFigure[i];
+                }
+                calculatorDisplayFigure = replacementDisplay;
+                numbersDisplay.innerHTML = calculatorDisplayFigure;
+            } else {
+                var placeholder = value.innerHTML;
+                calculatorDisplayFigure += String(placeholder);
+                numbersDisplay.innerHTML = calculatorDisplayFigure;
+            }
+        });
+        return value;
+    }) 
+
+    
+    
+
+
+
+
+    
+    
+
+    
 })
